72c558da3b8c673fa3629c5fa79ee7cb2d3d3e0d
MrMartinPet <martin_pet@hotmail.com>
2025-09-21 22:47:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,18 @@
-const CACHE = "viljas-laxa-v6";
+const CACHE = "viljas-laxa-cache-v2";
 const ASSETS = [
   "./",
   "./index.html",
-  "./words.html",
-  "./words.json",
   "./manifest.json",
-  "./sw.js",
-  "./icon-192-v4.png",
-  "./icon-512-v4.png"
+  "./icon-192.png",
+  "./icon-512.png",
+  "./sw.js"
 ];
-
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener("fetch", e => {

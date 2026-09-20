27ed24483a3c57b21f90b454ced035f7e9b714f6
MrMartinPet <martin_pet@hotmail.com>
2026-09-20 07:13:46 +0200

    Refresh offline cache for week 39

--- sw.js
+++ sw.js
@@ -1,20 +1,36 @@
-const CACHE = "viljas-laxa-cache-v5-week38";
+const CACHE = "viljas-laxa-cache-v6-week39";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
-  "./icon-512.png",
-  "./sw.js"
+  "./icon-512.png"
 ];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
   self.skipWaiting();
 });
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
   self.clients.claim();
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener("fetch", function(e){
+  if(e.request.mode === "navigate"){
+    e.respondWith(
+      fetch(e.request).then(function(r){
+        const copy = r.clone();
+        caches.open(CACHE).then(function(c){ c.put("./index.html", copy); });
+        return r;
+      }).catch(function(){
+        return caches.match("./index.html");
+      })
+    );
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
 });
